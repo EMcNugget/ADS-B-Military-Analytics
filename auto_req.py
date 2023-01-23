bcdff1dd6798ac5994e44710d7e840be0b4b7b2e
mrmcnuggetboi <ethan.d.seys@gmail.com>
2023-01-23 15:06:54 -0600

    update

--- auto_req.py
+++ auto_req.py
@@ -1,49 +1,72 @@
 import time
 import requests
-import logging
 import datetime
 import os
-import cred
+from threading import Thread
+from loguru import logger
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_HOST = os.getenv("API_HOST")
+DEP_DEPENDENCY = os.path.join(os.path.dirname(__file__), 'data\\')
 
 current_time = datetime.datetime.now().time()
 
-# Due to likely uptick in mil traffic, data is requested more frequently to catch more rare aircraft.
 if datetime.time(4, 0) <= current_time <= datetime.time(19, 0):
     variable = 350
-# Due to it being night in the UK and US traffic is winding down data is requested less frequently to save API requests.
 elif datetime.time(19, 0) < current_time <= datetime.time(23, 59):
     variable = 750
-# Transition period for mil aircraft activity in the UK.
 elif datetime.time(0, 1) < current_time <= datetime.time(3, 59):
     variable = 450
 else:
-    # Intermediate to catch other traffic during the downtimes of both US and UK traffic.
     variable = 550
-
-log_directory = "data\\"
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-logging.basicConfig(level=logging.INFO, filename=os.path.join(
-    log_directory, "adsb.json"), filemode='a')
 
 url = "https://adsbexchange-com1.p.rapidapi.com/v2/mil/"
 headers = {
-    "X-RapidAPI-Key": cred.API_KEY,
-    "X-RapidAPI-Host": cred.API_HOST
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": API_HOST
 }
 
+def dependenies():
+    if not os.path.exists(DEP_DEPENDENCY):
+        os.makedirs(DEP_DEPENDENCY)
+    elif not os.path.exists(DEP_DEPENDENCY + 'adsb.json'):
+        with open(DEP_DEPENDENCY + 'adsb.json', 'w') as data:
+            data.write('[]')
 
 def get_data():
     response = requests.request("GET", url, headers=headers)
-    logging.info(response.text)
+    return response.text
 
 
-while True:
+def auto_req():
+    while True:
+        try:
+            data = get_data()
+            with open(DEP_DEPENDENCY + 'adsb.json', 'a') as file:
+                file.write(str(data))
+            time.sleep(variable)
+        except Exception as e:
+            logger.error(e)
+        time.sleep(variable)
+
+def man_req():
+    user = input("Enter 'req' to request")
     try:
-        get_data()
-        print(f"Data requested at {datetime.datetime.now()}")
+        if user == "req":
+            data = get_data()
+            with open(DEP_DEPENDENCY + 'adsb.json', 'a') as file:
+                file.write(str(data))
+        else:
+            print("Invalid input")
     except Exception as e:
         print(e)
-        logging.error(e)
-    time.sleep(variable)
+
+if __name__ == "__main__":
+    dependenies()
+    Thread(target=auto_req).start()
+    Thread(target=man_req).start()
+
+
