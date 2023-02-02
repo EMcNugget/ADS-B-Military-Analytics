568f106cde880058105d753bf34f174094ed022c
EMcNugget <ethan.d.seys@gmail.com>
2023-02-02 13:55:41 -0600

    First run of data analysis. -Find the number of aircraft flown in decending order -Find interesting aircraft and return their data
    --Known issues--
    
    -list is unknown in the Analytics class. I'm not sure why. Debugging it currently.

--- src/logger_config.py
+++ src/logger_config.py
@@ -11,7 +11,7 @@ class Logger:
         self.formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.file_handler = logging.handlers.RotatingFileHandler(
-            f'{os.path.join(os.getcwd(), "logs/adsb_main.log")}', 
+            f'{os.path.join(os.getcwd(), "logs/adsb_main.log")}',
             maxBytes=1000000, mode='w', backupCount=5)
         self.file_handler.setFormatter(self.formatter)
         self.logger.addHandler(self.file_handler)
--- src/mongo_db.py
+++ src/mongo_db.py
@@ -1,8 +1,10 @@
 import datetime
 import os
 import json
+from dataclasses import dataclass, field
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import pandas as pd
 from .logger_config import log_app
 
 load_dotenv()
@@ -26,8 +28,41 @@ def get_mdb_data(date):
     with open(DEP_DEPENDENCY + f'final_adsb{date}.json', 'w', encoding='UTF-8') as file:
         try:
             if results is None:
-                log_main.critical(f"Data for {date} not found in database")
+                log_main.critical("Data for %s not found in database", date)
                 return
             json.dump(results['data'], file, indent=2)
         except TypeError:
             log_main.critical('Invalid date format. Please use YYYY-MM-DD format.')
+            return
+
+@dataclass
+class Analytics:
+    date: str = day
+    ac_type: list = field(default_factory= lambda: ['EUFI', 'F16', 'V22', 'F18S', 'A10', 'F35LTNG', 'S61', 'H64', 'F15'])
+    callsign: list = field(default_factory= lambda: ['AF1', 'AF2'])
+    
+    with open(DEP_DEPENDENCY + 'final_adsb2023-01-31.json', 'r', encoding='UTF-8') as file:
+        ac_data = json.load(file)
+        t_data_frame = pd.DataFrame(ac_data)
+        t_data_frame.fillna('No type available', inplace=True)
+
+    @classmethod
+    def for_data(cls, info_req):
+        """Date will be in YYYY-MM-DD format, provided by the UI"""
+
+        try:
+            log_main.info("%s data for %s fowarded to UI", info_req, cls.date)
+            return pd.value_counts(cls.t_data_frame[info_req]).to_dict()
+        except KeyError:
+            pass
+
+    @classmethod
+    def inter_ac(cls):
+        """Used for most interesting aircraft based on logic below"""
+        with open (DEP_DEPENDENCY + 'final_adsb2023-01-312.json', 'w', encoding='UTF-8') as file:
+            try:
+                json.dump(cls.t_data_frame[cls.t_data_frame['t'].isin(cls.ac_type)].to_dict(orient='records'), file, indent=2)
+            except KeyError as error:
+                log_main.critical(error)
+                return
+Analytics.inter_ac()
