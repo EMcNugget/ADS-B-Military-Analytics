c1db1a77fd38521fd255be7f1fcc5bd4209092cc
EMcNugget <ethan.d.seys@gmail.com>
2023-02-03 14:11:30 -0600

    Data update 3 -Added emergency aircraft searching -Added more debugging

--- src/analytics.py
+++ src/analytics.py
@@ -23,8 +23,8 @@ def rollover():
 
     while True:
         if time.strftime("%H:%M:%S", time.localtime()) == "23:59:00":
-            with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'a', encoding='UTF-8') as file:
-                file.write('{"end": "end"}\n]}')
+            with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'a', encoding='UTF-8') as rollover_file:
+                rollover_file.write('{"end": "end"}\n]}')
                 try:
                     # insert_data()  # Keep this as commented out while testing items
                     log_main.info("Data written to database")
@@ -35,8 +35,8 @@ def rollover():
             time.sleep(1)
 
 def load_pd_data():
-    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as file:
-        ac_df = json.load(file)
+    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as data_file:
+        ac_df = json.load(data_file)
         data = pd.DataFrame(ac_df['mil_data'])
         log_main.info("Data loaded from 'final_adsb%s.json'", day)
         ac_data_frame = data.drop_duplicates('hex')
@@ -48,20 +48,20 @@ def load_pd_data():
         log_main.info("Data written to 'final_adsb%s.json'", day)
 
 def insert_data():
-    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as file:
-        data = json.load(file)
+    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as mdb_i_file:
+        data = json.load(mdb_i_file)
         collection.insert_one({"_id": f"{day}", "data": data})
 
 def get_mdb_data(date):
     if not os.path.exists(DEP_DEPENDENCY):
         os.makedirs(DEP_DEPENDENCY)
     results = collection.find_one({"_id": f"{date}"})
-    with open(DEP_DEPENDENCY + f'final_adsb{date}.json', 'w', encoding='UTF-8') as file:
+    with open(DEP_DEPENDENCY + f'final_adsb{date}.json', 'w', encoding='UTF-8') as mdb_file:
         try:
             if results is None:
                 log_main.critical("Data for %s not found in database", date)
                 return
-            json.dump(results['data'], file, indent=2)
+            json.dump(results['data'], mdb_file, indent=2)
         except TypeError:
             log_main.critical('Invalid date format. Please use YYYY-MM-DD format.')
             return
@@ -71,9 +71,10 @@ class Analytics:
 
     ac_type = pd.Series(['EUFI', 'F16', 'V22', 'F18S', 'A10', 'F35LTNG', 'S61', 'H64', 'F15', 'AV8B', 'RC135'])
     callsign = pd.Series(['AF1', 'AF2'])
+    er_flags = pd.Series(['7700', '7600', 'general', 'lifeguard', 'minfuel', 'nordo', 'unlawful', 'downed', 'reserved']) 
 
-    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as file:
-        ac_data = json.load(file)
+    with open(DEP_DEPENDENCY + f'final_adsb{day}.json', 'r', encoding='UTF-8') as d_file:
+        ac_data = json.load(d_file)
         t_data_frame = pd.DataFrame(ac_data)
 
     @classmethod
@@ -87,11 +88,20 @@ class Analytics:
             pass
 
     @classmethod
-    def inter_ac(cls):
-        """Used for most interesting aircraft based on logic below"""
-        with open (DEP_DEPENDENCY + f'final_adsb{day}_stats.json', 'w', encoding='UTF-8') as file:
-            try:
-                json.dump(cls.t_data_frame[cls.t_data_frame['t'].isin(cls.ac_type)].to_dict(orient='records'), file, indent=2)
-            except KeyError as error:
-                log_main.critical(error)
-                return
+    def inter_ac(cls, row='t', data=ac_type):
+        """Used for special aircraft based on logic below"""
+        try:
+            place_holder = []
+            d_file = cls.t_data_frame[cls.t_data_frame[row].isin(data)].to_dict(orient='records')
+            if d_file == place_holder:
+                if data != cls.er_flags:
+                    raise KeyError
+                not_found = f'No aircraft with emergencies were found on {cls.date}'
+                log_main.info(not_found)
+                return not_found
+            else:
+                log_main.info("%s data from %s were forwarded to the UI", row, cls.date)
+                return d_file
+        except KeyError as error:
+            log_main.critical(error)
+            return
