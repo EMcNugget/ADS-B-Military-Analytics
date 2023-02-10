c95cce209b480d77541c53df73e05bad663622eb
EMcNugget <ethan.d.seys@gmail.com>
2023-02-10 16:38:46 -0600

    dev

--- adsb_main.sh
+++ adsb_main.sh
@@ -4,17 +4,6 @@ python -m ensurepip --upgrade
 
 pip install -r requirements.txt
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-npm install
-
-npm run build
-=======
-python -c "import app; app.run()" 
->>>>>>> parent of 11f59b2 (doc update)
-
->>>>>>> 59dfaf9af787f14f14d75de7cb00474735481d18
 if [ $? -eq 0 ]; then
     echo "Started, check log for more info on runtime events"
 else
