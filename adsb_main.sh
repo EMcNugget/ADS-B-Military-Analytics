40fda1136536a57e970ca89aa30e5dffc22e00ee
EMcNugget <ethan.d.seys@gmail.com>
2023-02-09 22:14:50 -0600

    dev

--- adsb_main.sh
+++ adsb_main.sh
@@ -4,6 +4,10 @@ python -m ensurepip --upgrade
 
 pip install -r requirements.txt
 
+npm install
+
+npm run build
+
 if [ $? -eq 0 ]; then
     echo "Started, check log for more info on runtime events"
 else
