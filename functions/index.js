b5ee720e29d09bb4fd498a12d1fcc1c89c54a8f2
EMcNugget <ethan.d.seys@gmail.com>
2023-02-13 12:42:36 -0600

    Fixed JS

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
+const functions = require("firebase-functions");
 const {exec} = require("child_process");
 const winston = require("winston");
 
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  transports: [
-    new winston.transports.File({filename: "firebase.log"}),
-  ],
+  transports: [new winston.transports.File({filename: "firebase.log"})],
 });
 
-logger.info("Starting the script...");
-
-exec("python app.py", (error, stdout, stderr) => {
+const main = exec("python app.py", (error, stdout, stderr) => {
   if (error) {
     logger.error(`exec error: ${error}`);
     return;
   }
   logger.info(`stdout: ${stdout}`);
   logger.error(`stderr: ${stderr}`);
+});
+
+exports.main = functions.https.onRequest((request, response) => {
+  response.exec(main);
 });
