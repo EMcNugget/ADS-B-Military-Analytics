5c164b10b03ea50ee5df74cb858e98ff9de836b5
EMcNugget <ethan.d.seys@gmail.com>
2023-02-15 09:38:47 -0600

    Revert "removed api from vite"
    This reverts commit a977401aac60b0debd56c4d8c32c318a296ed391.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: "public/index.html",
+    open: "/public/index.html",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      } 
     }
   }
-)
+})
