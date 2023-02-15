57eef9341d6bf212a42b16ee6959b5e8f62c8a97
EMcNugget <ethan.d.seys@gmail.com>
2023-02-15 09:51:55 -0600

    Gcloud

--- src/server/analytics.py
+++ src/server/analytics.py
@@ -26,6 +26,7 @@ config = {
 }
 app = Flask(__name__)
 app.config.from_mapping(config)
+app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 def load_pd_data(date: str=day):
     """Loads data from the JSON file and returns it as a pandas dataframe for further processing"""
