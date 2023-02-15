a977401aac60b0debd56c4d8c32c318a296ed391
EMcNugget <ethan.d.seys@gmail.com>
2023-02-14 21:48:27 -0600

    removed api from vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: "/public/index.html",
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      } 
+    open: "public/index.html",
     }
   }
-})
+)
