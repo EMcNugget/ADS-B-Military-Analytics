d53fc548508dfb280c7230da05a8ec293ab12a00
EMcNugget <ethan.d.seys@gmail.com>
2023-03-01 13:02:49 -0600

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,9 @@ export const API_URL = functions.https.onRequest((request, response) => {
 
   if (allowedOrigins.includes(request.headers.origin as string)) {
     response.set("Access-Control-Allow-Origin", request.headers.origin);
+    response.set("Access-Control-Allow-Methods", "GET, POST");
+    response.set("Access-Control-Allow-Headers", "Content-Type");
+    response.set("Access-Control-Allow-Credentials", "true");
     response.send(process.env.API_URL);
   } else {
     response.status(403).send("Forbidden");
