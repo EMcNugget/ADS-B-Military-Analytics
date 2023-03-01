82d95e1e4a2a4a6cc708b82efedffabbb57130c4
EMcNugget <ethan.d.seys@gmail.com>
2023-03-01 13:13:21 -0600

    Env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 
 export const API_URL = functions.https.onRequest((request, response) => {
-  const allowedOrigins = ["https://adsbmilanalytics.com", "https://www.adsbmilanalytics.com"];
+  const allowedOrigins = ["https://adsbmilanalytics.com", "https://www.adsbmilanalytics.com", "https://unified-dragon-378823.web.app", "https://unified-dragon-378823.firebaseapp.com"];
 
   if (allowedOrigins.includes(request.headers.origin as string)) {
     response.set("Access-Control-Allow-Origin", request.headers.origin);
